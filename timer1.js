68588df10ac6a7d16adeb269d46f0bcbb99e02eb
hannaohandrews <hannaohandrews@gmail.com>
2020-06-30 22:17:27 +0000

    done

--- timer1.js
+++ timer1.js
@@ -1,21 +1,12 @@
-//Implement an alarm clock / timer which will beep after a specified amount of time has passed. 
-//The user can specify an unlimited number of alarms using command line arguments
 
+let myArgs = process.argv.slice(2);
+// console.log('myArgs: ', myArgs);
 
-// setTimeout(() => {
-//   console.log('th3r3');
-// }, 1000);
-
-
-
-var myArgs = process.argv.slice(2);
-console.log('myArgs: ', myArgs);
-
-for (let i = 0 ; i < myArgs.length ; i++ ) {
+for (let i = 0; i < myArgs.length; i++) {
   setTimeout(() => {
     process.stdout.write('\x07');
   }, myArgs[i] * 1000);
-};
+}
 
 
 
